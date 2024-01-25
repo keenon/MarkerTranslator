94b91368d333e41b4faadd9ce0c77e40157d0b67
Keenon Werling <keenonwerling@gmail.com>
2024-01-24 17:43:33 -0800

    Increasing the size of the requested node, and requesting it for 24 hours instead of 12

--- src/slurm.sh
+++ src/slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 #SBATCH --job-name=marker_translator_training
-#SBATCH --time=12:00:00
-#SBATCH -c 16
+#SBATCH --time=24:00:00
+#SBATCH -c 32
 #SBATCH --mem=64000M
 #SBATCH --partition=bioe
 #SBATCH -G 1
@@ -10,4 +10,4 @@
 ml python/3.9.0
 
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
-python3 main.py train --device "cuda" --checkpoint-dir "$GROUP_HOME/marker-labeler/checkpoint-$TIMESTAMP" --dataset-home "$GROUP_HOME/data" --epochs 500 --data-loading-workers 16 --batch-size 16 --transformer-dim 256 --transformer-nheads 8 --transformer-nlayers 6
+python3 main.py train --device "cuda" --checkpoint-dir "$GROUP_HOME/marker-labeler/checkpoint-$TIMESTAMP" --dataset-home "$GROUP_HOME/data" --epochs 500 --data-loading-workers 30 --batch-size 16 --transformer-dim 256 --transformer-nheads 8 --transformer-nlayers 6
